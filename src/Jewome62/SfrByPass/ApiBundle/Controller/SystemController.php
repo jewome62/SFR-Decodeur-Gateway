c4bd21ad2993c0183aab24c3f2a38b502e1b7cd7
jewome62 <hello@jewome62.eu>
2014-07-13 22:44:50 +0200

    beginning system and routing

---
+++ src/Jewome62/SfrByPass/ApiBundle/Controller/SystemController.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace Jewome62\SfrByPass\ApiBundle\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+
+class SystemController extends Controller
+{
+    public function indexAction($method)
+    {
+        switch($method){
+            case 'getInfo' :
+                return $this->getInfo();
+                break;
+            default :
+                throw new NotFoundHttpException('Sorry not existing!');
+        }
+    }
+    
+    public function getInfo() {
+        return $this->render('Jewome62SfrByPassApiBundle:System:getInfo.xml.twig');
+    }
+}
